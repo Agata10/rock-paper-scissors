0d804bf18d88f20c48dbc08d60a805468cd826ec
Agata Deter <124964478+Agata10@users.noreply.github.com>
2023-06-30 14:20:16 -0600

    Add style.css and edit index.html. Add style to the webpage

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
-/*
-Get computer random answer:
-Create random number from 0 to 1 and multiplie it 
-with length of the array of possible answers(3) and
-round it.
-Get random pick from array of answers.
-*/
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+const result = document.querySelector('p.winner');
+
 
 function getComputerChoice() {
     let answers = ["Rock", "Paper", "Scissors"];
@@ -13,74 +11,68 @@ function getComputerChoice() {
     return randomAnswer;
 }
 
-/*Play one round of the game:
-WHEN get player choice make sure first letter will be
- upper and rest of word is lower.
- IF we have draw, say draw,
- return 0 for players score.
- ELSE IF player chose Paper,
-    IF computer chose Rock,
-    say "you won, Paper beats Rock",
-    return 1 for player's score.
-    ELSE, 
-    say, "you lose, Scissors beats Paper",
-    return 0 for player's score.
-ELSE IF player chose Rock,
-    IF computer chose Scissors,
-    say, "you won, Rock beats Scissors",
-    return 1 for player's score.
-    ELSE,
-     say, "you lose, Paper beats Scissors".
-     return 0 for player's score.
-ELSE IF player chose Scissors,
-    IF computer chose Paper,
-    say, "you won, Scissors beats Paper",
-    return 1 for player's score.
-    ELSE,
-     say, "you lose, Rock beats Scissors",
-     return 0 for player's score.
-  */
 function playRound(playerSelection, computerSelection) {
 
+const playerScoreText = document.querySelector('.playerScore');
+const computerScoreText = document.querySelector('.computerScore');
 playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-let playerScore = 0;
 
 if(playerSelection === computerSelection) {
-        alert(`Draw! You both picked ${computerSelection}`);
+        result.textContent = `Draw! You both picked ${computerSelection}`;
 } else if(playerSelection == "Paper") {
     if(computerSelection == "Rock") {
-        alert(`You won that round! ${playerSelection} beats ${computerSelection}`);
+        result.textContent = `You won that round! ${playerSelection} beats ${computerSelection}`;
         playerScore++;
+        playerScoreText.textContent = playerScore;
     } else {
-        alert(`You lose that round. Try harder! ${computerSelection} beats ${playerSelection}`);
+        result.textContent = `You lose that round. Try harder! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+        computerScoreText.textContent = computerScore;
     }
 } else if(playerSelection == "Rock") {
     if(computerSelection == "Scissors") {
-        alert(`You won that round! ${playerSelection} beats ${computerSelection}`);
+        result.textContent = `You won that round! ${playerSelection} beats ${computerSelection}`;
         playerScore++;
+        playerScoreText.textContent = playerScore;
     } else {
-        alert(`You lost that round. Try harder! ${computerSelection} beats ${playerSelection}`);
+        result.textContent = `You lose that round. Try harder! ${computerSelection} beats ${playerSelection}`;        computerScore++;
+        computerScoreText.textContent = computerScore;
     }
 } else if(playerSelection == "Scissors") {
     if(computerSelection == "Paper") {
-        alert(`You won that round! ${playerSelection} beats ${computerSelection}`);
+        result.textContent = `You won that round! ${playerSelection} beats ${computerSelection}`;
         playerScore++;
+        playerScoreText.textContent = playerScore;
     } else {
-        alert(`You lost that round. Try harder! ${computerSelection} beats ${playerSelection}`);
+        result.textContent = `You lose that round. Try harder! ${computerSelection} beats ${playerSelection}`;        computerScore++;
+        computerScoreText.textContent = computerScore;
     }
+   
 }
-    return playerScore;
 }
 
-/*Function which run 5 times the round and keeps score, 
-saying who is a winner or loser at the end of the game.
-PROMPT message to user and get his answer.
-CHECK who is winning and keep the value of score.
-Repeat 4 more times.
-IF player score is higher than or equal three,
-the winner is player,
-ELSE,
-the winner is computer.*/
+const buttons = document.querySelectorAll('button');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+    round++;
+    playRound(`${button.id}`, getComputerChoice()); 
+    if(round == 5)
+    if(playerScore > computerScore){
+       return result.textContent = "Congratulations!";
+    } else {
+       return result.textContent = "You lost";
+    }
+    
+    })
+});
+
+
+
+
+
+/*
 function game() {
     
     let recordOfScore = 0;
@@ -106,10 +98,5 @@ function game() {
     }
      
 }
-
-/*const playerSelection = "rock";
-const computreSelection = getComputerChoice();
-console.log(playRound(playerSelection, computreSelection));
 */
 
-console.log(game());
